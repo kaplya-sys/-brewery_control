06eca75925131c3f2a048a575a7d8cf9ba9b0093
Artem Alekseev <alekseevwork7@gmail.com>
2022-10-02 20:32:03 +0200

    Add template Tank

---
+++ webapp/tank/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class TitleBeer(Enum):
+    kellerbier = 'kellerbier'
+    dunkelbier = 'dunkelbier'
+    bropils = 'bropils'
+    wheatbeer = 'wheatbeer'
+    traditional_dark = 'traditional_dark'
+    traditional_light = 'traditional_light'
+    traditional_wheat = 'traditional_wheat'
+    cider = 'cider'
