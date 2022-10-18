cb82a172d295dc9a4bc3e9a33eb1ebd1db2662d5
Artem Alekseev <alekseevwork7@gmail.com>
2022-10-18 21:06:50 +0200

    done

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export FLASK_APP=webapp && flask db migrate && flask db upgrade && export FLASK_ENV=development && flask run --host 0.0.0.0
+export FLASK_APP=webapp && flask db upgrade && export FLASK_ENV=development && flask run --host 0.0.0.0
